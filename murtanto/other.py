e73b4d423ac039a38a034c5c7304e247feb3deb4
Salis Mazaya Miftah Malik <50546233+salismazaya@users.noreply.github.com>
2020-05-28 06:50:47 +0700

    keren

--- murtanto/other.py
+++ murtanto/other.py
@@ -2,31 +2,48 @@
 # 23 - 05 - 2020 23:18 (Malam Takbir)
 
 from .checker import check_login
+from .checker import check_argument
 from .output import Output
+from . import sorting
 from . import parsing
-import re
 
 @check_login
-def msgUrl(ses, next = None):
-	html = ses.session.get("https://mbasic.facebook.com/messages" if not next else next).text
-	data = parsing.parsing_href(html, "/read/")
-	next = parsing.parsing_href_regex(html, r"[?]pageNum.*selectable", one = True)
+def friend_request(ses, next = None):
+	html = ses.session.get("https://mbasic.facebook.com/friends/center/requests/" if not next else next).text
+	data = parsing.friendRequestParser(html)
+	return Output(items = data["items"], next = data["next"], html = html, session_number = ses.session_number)
+
+@check_login
+def friend_requested(ses, next = None):
+	html = ses.session.get("https://mbasic.facebook.com/friends/center/requests/outgoing/" if not next else next).text
+	data = parsing.parsing_href(html, "/cancel/?")
+	next = parsing.parsing_href(html, "?ppk=", one = True)
 	return Output(items = data, next = next, html = html, session_number = ses.session_number)
 
-def find_id_friend(ses, name):
-  html = ses.session.get("https://mbasic.facebook.com/search/people/?q={}&source=filter&isTrending=0".format(name)).text
-  url = parsing.parsing_href(html, "__xts__", one = True)
-  try:
-    html = ses.session.get(url).text
-    return (parsing.getMyName(html), re.search(r"owner_id=(\d+)", html).group(1))
-  except:
-    return
- 
-def find_id_group(ses, name):
-  html = ses.session.get("https://mbasic.facebook.com/search/groups/?q={}&source=filter&isTrending=0".format(name)).text
-  url = parsing.parsing_href(html, "__xts__", one = True)
-  try:
-    html = ses.session.get(url).text
-    return (parsing.getMyName(html), re.search(r"/groups/(\d+)", html).group(1))
-  except:
-    return
+@check_login
+@check_argument(["id"])
+def friend(ses, id = None, next = None):
+  if id.isdigit():
+    url = "https://mbasic.facebook.com/profile.php?id={}&v=friends".format(id)
+  else:
+    url = "https://mbasic.facebook.com/{}/friends".format(id)
+  html = ses.session.get(url if not next else next).text
+  data = parsing.listFriendParser(html)
+  return Output(items = data["items"], next = data["next"], html = data["html"], session_number = ses.session_number)
+
+def myFriend(ses, next = None):
+	return friend(ses, id = ses.id, next = next)
+
+@check_login
+def onlineFriend(ses, next = None):
+	out = []
+	html = ses.session.get("https://mbasic.facebook.com/buddylist.php").text
+	data = parsing.to_bs4(html).find_all("img", {"src":lambda x: "https://static.xx.fbcdn.net/rsrc.php/v3/ym/r/bzGumJjigJ0.png" in x})
+	data = [x.parent.parent for x in data]
+	del data[0]
+	for x in data:
+		a_class = x.find("a")
+		name = a_class.text
+		id_ = a_class["href"].split("fbid=")[1].split("&")[0]
+		out.append((name, id_))
+	return Output(items = out, html = html, session_number = ses.session_number)
